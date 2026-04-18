ceb25e1fb7cfca9a1a43404ff4d04f0bc9c08a04
MatyWarap <agueromatiase@gmail.com>
2026-04-17 22:48:00 -0300

    modify app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import StatsGrid from "./components/StatGrid";
+import { Box } from "@mui/material";
+
+const drawerWidth = 240;
 
 function App() {
-
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  )
+    <Box>
+      <Header />
+      <Box sx={{ padding: 3, marginLeft: "240px" }}>
+        <Sidebar />
+        <Box sx={{ padding: 3, marginLeft: "240px" }}>
+          <StatsGrid />
+        </Box>
+      </Box>
+    </Box>
+  );
 }
+
+export default App;
